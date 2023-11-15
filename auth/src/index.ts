20234322f128944f005818c63b91c6c9d55ac2ee
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-11-14 22:27:05 -0500

    scaffold ready

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get('/api/users/currentuser', async (req, res) => {
+    res.send('Hello');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
     
--- notes.md
+++ notes.md
@@ -4,3 +4,9 @@
 ### TypeScript
 
  - Create config file `tsc --init`
+
+### Flow
+
+ 1. Create micro-service directories and files
+ 2. Create Dockerfile and image
+ 3. Create Kubernetes Deployment and Service
