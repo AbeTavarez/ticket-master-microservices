dae7bb18c78fd1ec0b455f95f29e975871fda567
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-11-25 19:29:03 -0500

    password module

--- auth/src/utilities/password.ts
+++ auth/src/utilities/password.ts
@@ -4,20 +4,34 @@ import { promisify } from "util";
 const scryptAsync = promisify(scrypt);
 
 export class Password {
+    /**
+     * @description Hashes Password
+     * @param password 
+     * @returns hashed password with salt
+     */
   static async toHash(password: string) {
     // create salt
     const salt = randomBytes(8).toString("hex");
+
     // hash the password using the salt
     const buf = (await scryptAsync(password, salt, 64)) as Buffer;
 
     return `${buf.toString("hex")}.${salt}}`;
   }
 
+  /**
+   * @description Compares stored password with the supplied password
+   * @param storedPassword 
+   * @param suppliedPassword 
+   * @returns 
+   */
   static async compare(storedPassword: string, suppliedPassword: string) {
     // split the hashed password and the salt
     const [hashedPassword, salt] = storedPassword.split('.');
+
     // hash the provided password
     const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
     // compare hashes
     return buf.toString('hex') === hashedPassword;
   }
