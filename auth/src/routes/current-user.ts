64a6646ef1e0da2c4135a47703984680a651c2df
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-11-28 22:11:37 -0500

    current user tested

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,10 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get('/api/users/current-user', async(req, res) => {
-
-    // checks if request session does't includes a jwt token
-    if (!req.session?.jwt) {
-        return res.send({currentUser: null});
-    }
-
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        res.send({currentUser: payload});
-    } catch (err) {
-        res.send({currentUser: null});
-    }
+router.get("/api/users/current-user", currentUser, async (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
-export {router as currentUserRouter};
+export { router as currentUserRouter };
