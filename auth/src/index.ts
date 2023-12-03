0f6cd6924ad3f042286c95e175ea7d62e65c8554
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-12-03 16:40:13 -0500

    jest config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,49 +1,6 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-// import morgan from "morgan";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-const PORT = 3000;
-
-// App Settings
-
-// trust ingress nginx proxy traffic
-app.set("trust proxy", true);
-
-// Middlewares
-// app.use(morgan("dev"));
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false, // disable encryption
-    //secure: true, // HTTPS connections only
-  }),
-);
-
-// Routes
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// 404 Not Found
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-// Error Handling Middleware
-app.use(errorHandler);
-
-// Main
+import { app } from "./app";
+//
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT must be defined");
@@ -54,8 +11,8 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
-  app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  app.listen(3000, () => {
+    console.log(`Listening on port: ${3000}`);
   });
 };
 
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,34 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: any;
+
+// === Initial Setup
+beforeAll(async () => {
+    // create instance of mongo
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    // connect to mongo
+    await mongoose.connect(mongoUri);
+});
+
+// === Runs before each of the tests
+beforeEach(async () => {
+    // get all collections
+    const collections = await mongoose.connection.db.collections();
+    
+    // Deletes all collections (reset)
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+
+// === Runs after all of the tests are completed
+afterAll(async () => {
+    // stops mongo server
+    await mongo.stop();
+    // disconnects from server
+    await mongoose.connection.close();
+})
