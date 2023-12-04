31b43d8d6a0b4bebda9fdf19aaf334e17f76e42b
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-12-03 19:28:14 -0500

    signin tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,57 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails when an email does not exist", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password1$",
+    })
+    .expect(400);
+});
+
+it("fails when an incorrect password is supplied", async () => {
+  // creates a new account
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password1$",
+    })
+    .expect(201);
+
+  // now it tries to sign in with wrong password
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password1",
+    })
+    .expect(400);
+});
+
+it("sign in and responds with a cookie", async () => {
+  // creates a new account
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+    console.log(response.body);
+    
+
+  // now it tries to sign in with wrong password
+  const res =  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  expect(res.get("Set-Cookie")).toBeDefined();
+});
