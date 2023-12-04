afaf60404c8ba851a5aa1dc34b4c2ae449dc16da
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-12-03 20:31:31 -0500

    global sign in function for testing

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+// tells TS about global function
+declare global {
+    var signin: () => Promise<string[]>;
+  }
 
 let mongo: any;
 
@@ -34,4 +40,20 @@ afterAll(async () => {
     await mongo.stop();
     // disconnects from server
     await mongoose.connection.close();
-})
+});
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const authRes = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+    const cookie = authRes.get("Set-Cookie");
+    return cookie;
+}
