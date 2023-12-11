4c06e3b6577f880f27455595b5fbb6d132f9aa20
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-12-11 14:08:35 -0500

    sendrequest callback function for onsuccess

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,14 +1,19 @@
 import {useState} from 'react';
 import axios from 'axios';
 
-export default function useRequest({url, method, body}) {
+export default function useRequest({url, method, body, onSuccess}) {
     const [errors, setErrors] = useState(null);
 
     const sendRequest = async () => {
         try {
             const res = await axios[method](url, body);
+
+            if (onSuccess) {
+              onSuccess(res.data);
+            }
+            
             return res.data;
-        } catch (e) {
+        } catch (err) {
             setErrors(
                 <div className="alert alert-danger">
                 <h4>Ooops...</h4>
